9c777d04f3c98f5438f9bc0dd9028621ce303824
gary-dev-aus <gary@gary-dev.com>
2022-11-04 11:21:08 +1100

    temp

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,6 @@
 <script>
   import "../app.css";
   import Header from "../lib/components/header.svelte";
-
-  import { PUBLIC_GRAPHQL_ENDPOINT as graphqlEndpoint } from "$env/static/public";
-
-  import { ApolloClient, InMemoryCache } from "@apollo/client/core";
-  import { setClient } from "svelte-apollo";
-
-  export const client = new ApolloClient({
-    uri: graphqlEndpoint,
-    cache: new InMemoryCache(),
-  });
-
-  setClient(client);
 </script>
 
 <Header />
