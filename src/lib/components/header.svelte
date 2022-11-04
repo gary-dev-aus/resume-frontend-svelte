3abde46410e16da4a594a64ecc5588de79118539
gary-dev-aus <gary@gary-dev.com>
2022-11-04 11:19:30 +1100

    Testing apollo

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+    <a href="/"><h2>Gary Zhang</h2> </a>
+    <a href="/portfolio">portfolio</a><a href="/knowledge">knowledge</a><a
+      href="/contact">contact</a
+    >
+  </nav>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
+<script>
+  import { gql } from "@apollo/client/core";
+  import { query } from "svelte-apollo";
+
+  const HOMEPAGE = gql`
+    query GetHomepage {
+      homepage {
+        data {
+          attributes {
+            heroMain
+            heroSupport
+          }
+        }
+      }
+    }
+  `;
+
+  const homepage = query(HOMEPAGE);
+  // let data;
+
+  // homepage.subscrive((data) => {
+  //   data = data;
+  // });
+
+  console.log(homepage);
+</script>
+
 <h1>homepage</h1>
+<!-- {#if !$homepage.loading && !$homepage.error} -->
+{homepage.data?.attributes.heroMain}
+<!-- {/if} -->
