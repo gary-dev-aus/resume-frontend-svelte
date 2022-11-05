50e534e0983117a3bdaf0456fad8c0a2f70d3768
gary-dev-aus <gary@gary-dev.com>
2022-11-05 11:40:59 +1100

    Building out knowledge page and image component.

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,7 +1,29 @@
 <script>
-  export let something = "default";
+  import { PUBLIC_STRAPI_ENDPOINT as urlEndpoint } from "$env/static/public";
+
+  // Props
+  export let image = undefined;
+
+  image.url = image.url.replace("/uploads/", "");
+  const urlUpload = urlEndpoint + "/uploads/";
+
+  const breakpoints = {
+    sm: "480",
+    md: "800",
+    lg: "1000",
+  };
+
+  console.log(image, urlUpload + image.url);
 </script>
 
-<div>
-  {something}
-</div>
+{#if image.url.endsWith(".svg")}
+  <!-- svg -->
+  <img src={urlUpload + image.url} alt={image.alternativeText} />
+{:else}
+  <!-- img -->
+  <img
+    srcset={`${urlUpload}small_${image.url} ${breakpoints.sm}w, ${urlUpload}medium_${image.url} ${breakpoints.md}w, ${urlUpload}large_${image.url} ${breakpoints.lg}w`}
+    src={urlUpload + image.url}
+    alt={image.alternativeText}
+  />
+{/if}
--- src/routes/knowledge/+page.svelte
+++ src/routes/knowledge/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { knowledgeStore } from "$lib/stores/knowledgeStore";
-  import Image from "$lib/components/image";
+  import Image from "$lib/components/image.svelte";
 
   let knowledgeTypes = [];
 
@@ -8,7 +8,7 @@
     ...new Set($knowledgeStore.map((knowledge) => knowledge.type)),
   ];
 
-  // console.log($knowledgeStore, knowledgeTypes, knowledgeTypes.length >= 1);
+  // console.log($knowledgeStore);
 </script>
 
 <h1>knowledge</h1>
@@ -18,11 +18,11 @@
       {knowledgeType}
     </h2>
     <ul>
-      {#each $knowledgeStore as knowledge (knowledge.id)}
+      {#each $knowledgeStore.filter((knowledge) => knowledge.type === knowledgeType) as knowledge (knowledge.id)}
         <li>
           <p>{knowledge.score}</p>
           <h3>{knowledge.title}</h3>
-          <Image />
+          <Image image={knowledge.logo} />
         </li>
       {/each}
     </ul>
